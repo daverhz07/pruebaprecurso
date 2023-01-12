c993d13b96cfd65e9242b44f9141d5d29f557234
David Eduardo Rueda Hernandez <dave.ruedahz@gmail.com>
2023-01-12 09:58:06 -0600

    ruta holayadios añadida

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/hola' , (req, res) =>{
+    res.send('ruta Hola');
+});
+
+app.get('/adios' , (req, res) =>{
+    res.send('ruta Adios');
+});
 
 
 app.listen(3000, () => {
