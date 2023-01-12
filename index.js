097fed7dc5ed8840ae51110f98de9dd2add27654
David Eduardo Rueda Hernandez <dave.ruedahz@gmail.com>
2023-01-12 17:08:56 -0600

    nueva ruta de registro

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/adios' , (req, res) =>{
     res.send('ruta Adios');
 });
 
+app.post('/registro' , (req, res) =>{
+    res.send('recibiendo informacion de registro');
+});
+
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
